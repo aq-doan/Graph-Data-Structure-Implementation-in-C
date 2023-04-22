c7b49cd2e50369ba4bb9aa03c682fe8d89c3106a
Anh Doan <Anh.Doan@utas.edu.au>
2023-04-22 22:35:36 +1000

    implement testing

--- Week8 Lab/main.c
+++ Week8 Lab/main.c
@@ -5,7 +5,24 @@
 #include <stdbool.h>
 #include <string.h>
 #include "graph.h"
-
+int* create_in_degrees(int a) {
+    int* in_degrees = malloc(sizeof(int) * a);
+    for (int i = 0; i < a; i++) {
+        in_degrees[i] = 0;
+        return in_degrees;
+    }
+}
+void destroy_graph(Graph G) {
+    for (int i = 0; i < G.V; i++) {
+        EdgeNodePtr edge_node = G.edges[i].head;
+        while (edge_node != NULL) {
+            EdgeNodePtr temp = edge_node;
+            edge_node = edge_node->next;
+            free(temp);
+        }
+    }
+    free(G.edges);
+}
 int main() {
 	Graph G; // define a new graph
 	scanf("%d", &G.V);// read the input.txt
@@ -31,7 +48,29 @@ int main() {
 
 		}
 	}
+    //initialise array and set all to 0
+    int* in_degrees = create_in_degrees(&G.V);
 
+    /*loop through all of the edges in the graph and increment
+    *the in - degree for the to - vertex of each edge
+    */
+    for (int i = 0; i < G.V; i++) {
+        EdgeNodePtr node = G.edges[i].head;
+        while (node != NULL) {
+            in_degrees[node->edge.to_vertex]++;
+            node = node->next;
+        }
+    }
+
+    //print
+    for (int count = 0; count < G.V; count++) {
+        printf("Vertex %d has in-degree %d\n", count, in_degrees[count]);
+    }
+
+    //freeing memory
+    free(in_degrees);
+
+    destroy_graph(G);
 
 
 
